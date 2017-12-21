8b673030669d30dc37a8ce0c8c1b157285b0e3bb
Oren Bissick <obissick@greathealthworks.com>
2017-12-21 15:33:27 -0500

    restructure code and added thread for proxy and api.

--- src/main/java/com/orenbissick/GaleraProxy/GaleraProxyApplication.java
+++ src/main/java/com/orenbissick/GaleraProxy/GaleraProxyApplication.java
@@ -15,50 +15,66 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GaleraProxyApplication {
 	private static final String USER_AGENT = "Mozilla/5.0";
+    static Configuration prop = new Configuration();
+	static int choosenServer;
+    static int lastServer = 0;
+    
+    static String hosts[] = prop.getRIP();
+    static int remoteport = Integer.parseInt(prop.getRPort());
+    static int localport = Integer.parseInt(prop.getLPort());
+    static DBNode [] nodes = new DBNode[hosts.length];
 	public static void main(String[] args) {
 		SpringApplication.run(GaleraProxyApplication.class, args);
-        Configuration prop = new Configuration();
-        ServerSocket server = null;
-        int lastServer = 0;
+        
         try{
             // and the local port that we listen for connections on
-            String hosts[] = prop.getRIP();
-            int remoteport = Integer.parseInt(prop.getRPort());
-            int localport = Integer.parseInt(prop.getLPort());
-            DBNode [] nodes = new DBNode[hosts.length];
+            
             for(int i = 0; i < hosts.length; i++) {
                 nodes[i] = new DBNode(hosts[i],"jdbc:mariadb://"+hosts[i]+":"+remoteport,prop.getUser(),prop.getPassword());
-                try {
-					sendPost(nodes[i].getIP(),nodes[i].getConnectionsSinceStart(),nodes[i].getConnections(),nodes[i].getState());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
             }
-            server = new ServerSocket(localport);
-            while(true){
-                int choosenServer = chooseServer(prop.getLoadType(),nodes,lastServer);
-                System.out.println("Sending connection to " + hosts[choosenServer] + ":" + remoteport
-                    + " on port " + localport);
-                new ThreadProxy(server.accept(), hosts[choosenServer], remoteport);
-                nodes[choosenServer].incConnectionsSinceStart();
-                try {
-					sendPost(nodes[choosenServer].getIP(),nodes[choosenServer].getConnectionsSinceStart(),nodes[choosenServer].getConnections(),nodes[choosenServer].getState());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-                lastServer = choosenServer;
-            }
+            final ServerSocket server = new ServerSocket(localport);
+            
+            Thread proxyThread = new Thread(
+            		() -> {
+                    	while(true){
+                    		runProxy(server,remoteport,localport,hosts,nodes);
+                    	}
+            });
+            proxyThread.start();
+                
+            Thread apiThread = new Thread(
+                    () -> {
+                    	while(true) {
+                    		for(int i = 0; i < hosts.length; i++) {
+                                nodes[i] = new DBNode(hosts[i],"jdbc:mariadb://"+hosts[i]+":"+remoteport,prop.getUser(),prop.getPassword());
+                                try {
+                					sendPost(nodes[i].getIP(),nodes[i].getConnectionsSinceStart(),nodes[i].getConnections(),nodes[i].getState());
+                				} catch (Exception e) {
+                					e.printStackTrace();
+                				}
+                            }
+                    	}
+                });
+            apiThread.start();
+            
         }catch(IOException | IllegalArgumentException e){
             System.out.println(e.getMessage());   
         }
+	}
+	
+	public static void runProxy(ServerSocket server, int remoteport, int localport, String [] hosts, DBNode[] nodes) {
+                	
+        choosenServer = chooseServer(prop.getLoadType(),nodes,lastServer);
+        System.out.println("Sending connection to " + hosts[choosenServer] + ":" + remoteport
+            + " on port " + localport);
         try {
-			server.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			new ThreadProxy(server.accept(), hosts[choosenServer], remoteport);
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
+        nodes[choosenServer].incConnectionsSinceStart();
+        
+        lastServer = choosenServer;
 	}
 	
 	public static int chooseServer(String type, DBNode[] nodes, int last){
